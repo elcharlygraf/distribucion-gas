171a796328b172cfa870ea724b24382af6a9ab37
elcharlygraf <carlox16@gmail.com>
2016-03-27 13:14:14 -0500

    Estructura

---
+++ REST/RESTPROYECT/RESTPROYECT/DetallePedidos.svc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace RESTPROYECT
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "DetallePedidos" en el código, en svc y en el archivo de configuración a la vez.
+    // NOTA: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione DetallePedidos.svc o DetallePedidos.svc.cs en el Explorador de soluciones e inicie la depuración.
+    public class DetallePedidos : IDetallePedidos
+    {
+        public void DoWork()
+        {
+        }
+    }
+}
---
+++ REST/RESTPROYECT/RESTPROYECT/Productos.svc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace RESTPROYECT
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "Productos" en el código, en svc y en el archivo de configuración a la vez.
+    // NOTA: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione Productos.svc o Productos.svc.cs en el Explorador de soluciones e inicie la depuración.
+    public class Productos : IProductos
+    {
+        public void DoWork()
+        {
+        }
+    }
+}
